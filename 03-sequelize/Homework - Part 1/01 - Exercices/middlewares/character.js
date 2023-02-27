c6d52889888dd7e76ca8c47fb8b9ec693ace74c6
Daniel Castillo <Daniel.302009@hotmail.com>
2023-02-27 15:04:22 -0500

    HW 02 SEQUELIZE

--- 03-sequelize/Homework - Part 1/01 - Exercices/middlewares/character.js
+++ 03-sequelize/Homework - Part 1/01 - Exercices/middlewares/character.js
@@ -1,5 +1,82 @@
 const { Router } = require("express");
-const { Op, Character, Role } = require("../db");
+const { Character } = require("../db");
 const router = Router();
+
+router.post('/', async(req, res) => {
+    try {
+        const {code, name, age, race, hp, mana, date_added} = req.body;
+
+        if(!code || !name || !age || !race || !hp || !mana){
+            return res.status(404).send('Falta enviar datos obligatorios')
+        }
+
+        const newCharacter = await Character.create({
+            code,
+            name,
+            age,
+            race,
+            hp,
+            mana,
+            date_added
+        })
+
+        return res.status(201).json(newCharacter)
+    } catch (error) {
+        return res.status(404).send('Error en alguno de los datos provistos')
+    }
+})
+
+router.get('/', async(req, res) => {
+    try {
+        const {race, age} = req.query;
+
+        const condition = {};
+        const where = {};
+
+        if(race) where.race = race;
+
+        if(age) where.age = age;
+
+        condition.where = where;
+
+        const characters = await Character.findAll(condition);
+
+        return res.status(200).json(characters)
+
+    } catch (error) {
+        return res.status(404).send('Error en alguno de los datos provistos')
+    }
+})
+
+router.get('/:code', async(req, res) => {
+    try {
+        const { code } = req.params;
+
+        const characterByPk = await Character.findByPk(code);
+
+        if(!characterByPk) throw new Error(`El codigo ${code} no corresponde a un personaje existente`);
+
+        return res.status(200).json(characterByPk);
+    } catch (error) {
+        return res.status(404).send(error.message)
+    }
+})
+
+router.put('/:attribute', async(req, res) => {
+    try {
+        const { attribute } = req.params;
+        const { value } = req.query;
+
+        await Character.update({[attribute]: value}, {
+            where:{
+                [attribute]: null
+            }
+        })
+
+        return res.status(200).send('Personajes actualizados');
+    } catch (error) {
+        return res.status(404).send(error.message)
+    }
+})
 
 module.exports = router;
